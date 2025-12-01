84e985023c4665417173a76e286feff8864b3a7a
KhushbooMehto <khushboo200022@gmail.com>
2025-12-01 23:59:09 +0530

    my_Modules

---
+++ demo_module.py
@@ -0,0 +1,8 @@
+# this is my demo module
+
+name = "khushboo"
+
+def greet_fun():
+    return "Hello",name,"this is my demo module"
+
+print(greet_fun())
---
+++ my_module.py
@@ -0,0 +1,5 @@
+
+#importing my demo module
+import demo_module
+
+demo_module.greet_fun()
