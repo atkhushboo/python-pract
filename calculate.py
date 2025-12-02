6582bb7f39828b1d45061b9cbd02755dce230782
KhushbooMehto <khushboo200022@gmail.com>
2025-12-02 19:21:58 +0530

    my_cal-module

---
+++ calculate.py
@@ -0,0 +1,21 @@
+# importing my_cal module
+
+from my_cal import *
+
+num1=int(input("enter first :- "))
+num2=int(input("enter second :- "))
+
+addition = add(num1,num2)
+print(addition)
+
+subtracion = sub(num1,num2)
+print(subtracion)
+
+multiplication = mul(num1,num2)
+print(multiplication)
+
+division = div(num1,num2)
+print(division)
+
+
+
